593de929718c1f53f9982274b55f5b282cbc792a
Jaime Roncal <55926526+jaimeroncal@users.noreply.github.com>
2022-04-16 13:51:30 +0200

    Todo menos obtención de localización

--- p5SID/app/src/main/java/es/unizar/eina/p5sid/AsyncTaskMain.java
+++ p5SID/app/src/main/java/es/unizar/eina/p5sid/AsyncTaskMain.java
@@ -1,49 +1,134 @@
 package es.unizar.eina.p5sid;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.media.Image;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Collections;
+import java.util.List;
 
-public class AsyncTaskMain extends AsyncTask<AsyncTaskMain.Posicion, Void, Integer>  {
+public class AsyncTaskMain extends AsyncTask<Location, Void, AsyncTaskMain.Imagen>  {
 
-    static class Posicion {
-        float latitud;
-        float longitud;
-    }
     private MainActivity mActivity = null;
+
+    // PARAMETRO SALIDA
+    static class Imagen {
+        Bitmap imagen;
+        String titulo;
+        String latitud;
+        String longitud;
+    }
+
+    // CLASES PARA PARSEAR EL JSON CON GSON
+    static class Photo {
+        String id;
+        String owner;
+        String title;
+        String url_s;
+        String latitude;
+        String longitude;
+    }
+
+    static class Photos {
+        Integer page;
+        Integer pages;
+        Integer perpage;
+        String total;
+        List<Photo> photo;
+    }
+
+    static class Result {
+        Photos photos;
+        String stat;
+    }
+
     public AsyncTaskMain(MainActivity activity)
     {
         attach(activity);
     }
+
     @Override
-    protected Integer doInBackground(Posicion... params)
+    protected Imagen doInBackground(Location... params)
     {
         HttpURLConnection connection;
-        float latitud = params[0].latitud;
-        float longitud = params[0].longitud;
+        Double latitud = params[0].getLatitude();
+        Double longitud = params[0].getLongitude();
+        String url =  "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key="+mActivity.getString(R.string.apiFlickr)+"&lat="+latitud.toString()+"&lon="+longitud.toString()+"&radius=5&radius_units=km&sort=date-posted-desc&extras=geo,url_s&format=json&nojsoncallback=1";
         try {
-            connection = (HttpURLConnection) new URL("params[0]").openConnection();
-            return connection.getResponseCode();
+            // GET LIST
+            connection = (HttpURLConnection) new URL(url).openConnection();
+            InputStreamReader reader = new InputStreamReader(connection.getInputStream());
+            Gson gson = new Gson();
+            Result resultado = gson.fromJson(reader, Result.class);
+
+            // GET IMAGE, TITLE AND LOCATION with url_s not null
+            int idPhoto = 0;
+            String urlImagen = resultado.photos.photo.get(idPhoto).url_s;
+            while ((urlImagen==null || urlImagen.equals("")) && idPhoto < resultado.photos.perpage){
+                idPhoto ++;
+                urlImagen = resultado.photos.photo.get(idPhoto).url_s;
+            }
+
+            Imagen imagen = new Imagen();
+            imagen.titulo = resultado.photos.photo.get(idPhoto).title;
+            imagen.latitud = resultado.photos.photo.get(idPhoto).latitude;
+            imagen.longitud = resultado.photos.photo.get(idPhoto).longitude;
+
+            InputStream in = new java.net.URL(urlImagen).openStream();
+            imagen.imagen = BitmapFactory.decodeStream(in);
+
+            return imagen;
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return -1;
+        return new Imagen();
     }
+
     @Override
-    protected void onPostExecute(Integer integer)
+    protected void onPostExecute(Imagen imagen)
     {
         if (mActivity == null)
             Log.i("MyAsyncTask", "Me salto onPostExecute() -- no hay nueva activity");
-        else
-            mActivity.setupAdapter(integer);
+        else {
+            // INVISIBLE MESSAGE
+            TextView message = (TextView) mActivity.findViewById(R.id.empty);
+            message.setVisibility(View.GONE);
+            // SET TITLE
+            TextView mTitulo = (TextView) mActivity.findViewById(R.id.titulo);
+            mTitulo.setText(imagen.titulo);
+            mTitulo.setVisibility(View.VISIBLE);
+            // SET LAT LONG
+            TextView mLatLong = (TextView) mActivity.findViewById(R.id.latlong);
+            mLatLong.setText("Latitud: "+imagen.latitud+", Longitud: "+imagen.longitud);
+            mLatLong.setVisibility(View.VISIBLE);
+            // SET IMAGE
+            ImageView mImagen = (ImageView) mActivity.findViewById(R.id.imagen);
+            mImagen.setImageBitmap(imagen.imagen);
+            mImagen.setVisibility(View.VISIBLE);
+
+            mActivity.setupAdapter(0);
+        }
     }
+
     void detach()
     {
         this.mActivity = null;
     }
+
     void attach(MainActivity activity)
     {
         this.mActivity = activity;
--- p5SID/app/src/main/java/es/unizar/eina/p5sid/MainActivity.java
+++ p5SID/app/src/main/java/es/unizar/eina/p5sid/MainActivity.java
@@ -1,42 +1,104 @@
 package es.unizar.eina.p5sid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private final static String TAG = "MainActivity";
     private AsyncTaskMain myTask = null;
+    private GoogleMap mMap;
+    private FusedLocationProviderClient fusedLocationClient;
+    private Location localizacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        // Create location client
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        // Botón buscar
+        ImageButton botonAnadir = (ImageButton) findViewById(R.id.search);
+        botonAnadir.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                llamarATarea();
+            }
+        });
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+    }
+
+    public void llamarATarea() {
         // Para obtener el valor devuelto en onRetainCustomNonConfigurationInstance
         myTask = (AsyncTaskMain) getLastCustomNonConfigurationInstance();
         if (myTask == null) {
             // Evita crear una AsyncTask cada vez que, por ejemplo, hay una rotación
             Log.i(TAG, "onCreate: About to create MyAsyncTask");
+
+            // get location
+            getLocation();
+
             myTask = new AsyncTaskMain(this);
-            AsyncTaskMain.Posicion pos = new AsyncTaskMain.Posicion();
-            pos.latitud = 0;
-            pos.longitud = 0;
-            myTask.execute(pos);
-        }
-        else
+            myTask.execute(localizacion);
+        } else
             myTask.attach(this);
+        Toast.makeText(this, "Buscando en Flickr", Toast.LENGTH_LONG).show();
     }
+
+    public void getLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationClient.getLastLocation().addOnSuccessListener(this,
+                new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            localizacion.setLatitude(location.getLatitude());
+                            localizacion.setLongitude(location.getLongitude());
+                        }
+                    }
+                }
+        );
+    }
+
     @Override
     public Object onRetainCustomNonConfigurationInstance()
     {
@@ -51,5 +113,11 @@ public class MainActivity extends AppCompatActivity {
         if (integer != -1)
             Toast.makeText(MainActivity.this,
                     "Codigo de respuesta: " + integer, Toast.LENGTH_LONG).show();
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
